df9df4a23a9bdc6b7e2fb7f0d46652af0d8888ac
levinzx <levinxz@gmail.com>
2024-09-20 14:19:17 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ print("This is NASA Git/Python course repository on GitHub.")
 
 def print_message(name='GitHub'):
   print(f"Welcome to the {name} tutorial!")
-  print_message()
+  
+print_message()
